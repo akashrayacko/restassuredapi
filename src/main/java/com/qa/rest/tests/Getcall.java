d957f0a53c2f1f37e2b10c67e9e3c41af90dfb50
akash.ray <akash.ray_int@acko.tech>
2022-10-14 01:23:00 +0530

    https://github.com/akashrayacko/restassuredapi.git

---
+++ src/main/java/com/qa/rest/tests/Getcall.java
@@ -0,0 +1,36 @@
+package com.qa.rest.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import  static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class Getcall {
+
+    @Test
+    public void test_numberofCircuitsFor2017_season() {
+
+        //given()
+        //when()
+        //then()
+        //assert()
+
+                given().
+                when().
+                get("http://ergast.com/api/f1/2017/circuits.json").
+                then().
+                        assertThat().
+
+                        statusCode(200).
+
+                        and().
+
+                        body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+
+                       // and().
+
+                       // header("content-length", equalTo("4551"));
+
+
+    }
+}
